ead6788e2ce075d0568e3bf71fe7136f23acd134
mam5588 <mam5588@g.rit.edu>
2020-01-03 11:47:07 -0500

    Getting used to environment

--- src/main/java/b404/Login.java
+++ src/main/java/b404/Login.java
@@ -2,6 +2,7 @@ package b404;
 
 import javax.crypto.SecretKey;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -18,12 +19,14 @@ public class Login {
     @Produces("application/json")
     public void submit(@FormParam("value") String value) {
 
-
-
+        /*
         SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         String jws = Jwts.builder().setSubject("Joe").signWith(SignatureAlgorithm.HS256, key).compact();
 
         assert Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody().getSubject().equals("Joe");
+        */
+
+        return Response.ok("{\"success\":\"You have logged in!\"}").build();
 
     }
 
