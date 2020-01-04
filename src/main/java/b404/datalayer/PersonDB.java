252d3923666f7f15851e0e4f7776c510bb3a5cc1
Matt <mam5588@g.rit.edu>
2020-01-04 01:51:08 -0500

    Set up structure for making database call. Still need to get url and credentials for database and implement object mapping for database response information.

--- src/main/java/b404/datalayer/PersonDB.java
+++ src/main/java/b404/datalayer/PersonDB.java
@@ -2,12 +2,52 @@ package main.java.b404.datalayer;
 
 import main.java.b404.utility.Person;
 
+import javax.ws.rs.InternalServerErrorException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class PersonDB {
+    private Connection conn;
+
+    /**
+     * Connect to the database
+     * @return status Returns true if connected to the database, otherwise false.
+     */
+    public boolean connect(){
+        conn = null;
+
+        //TODO: communicate on what these values should be and how best to store them
+        String userName = "b404";
+        String password = "b404";
+        String url = null;
+
+        try{
+            conn = DriverManager.getConnection(url, userName, password);
+        }
+        //return false on error connecting
+        catch(SQLException sqle){
+            return false;
+        }
+
+        //return true or false based on whether a connection was successfully established
+        return (conn != null);
+    }
 
     public Person getPersonByName(String name) throws SQLException {
-        //TODO make DB call for person
+        if(!this.connect()){
+            throw new SQLException("Error connecting to database");
+        }
+
+        String query = "SELECT * FROM person WHERE person.name = ?";
+        PreparedStatement preparedStatement = this.conn.prepareStatement(query);
+
+        preparedStatement.setString(1, name);
+        preparedStatement.executeQuery();
+
+        //TODO: map database response to a Person object
+
         return new Person();
     }
 }
