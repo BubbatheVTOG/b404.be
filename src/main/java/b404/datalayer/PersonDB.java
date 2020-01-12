a796068a80bd9b83c74be1e8a37c92df83aca0fc
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-01-12 14:29:22 -0500

    Testing Environment manager issue

--- src/main/java/b404/datalayer/PersonDB.java
+++ src/main/java/b404/datalayer/PersonDB.java
@@ -16,10 +16,10 @@ public class PersonDB {
     private String password;
 
     public PersonDB(){
-        this.env = new EnvManager();
+        //this.env = new EnvManager();
 
         this.driver = "org.mariadb.jdbc.Driver";
-        this.url = "jdbc:mariadb://" + env.getValue("DB_NAME") + ":3306/venture_creations?allowPublicKeyRetrieval=true";
+        //this.url = "jdbc:mariadb://" + env.getValue("DB_NAME") + ":3306/venture_creations?allowPublicKeyRetrieval=true";
 
         //TODO: communicate on what these values should be and how best to store them
         this.user = "b404";
