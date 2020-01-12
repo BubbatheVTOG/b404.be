581d1c078fd8746c0db1a37a7fe457ab551e7ce5
Burak Engin <bxe5644.rit.edu>
2020-01-12 11:18:10 -0500

    This is adding in the SHA-512 hashing function but commenting it out until needed. This is to simplify testing.

--- src/main/java/b404/security/PasswordEncryption.java
+++ src/main/java/b404/security/PasswordEncryption.java
@@ -1,7 +1,27 @@
 package b404.security;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordEncryption {
     public static String encrypt(String password){
         return password;
+        /*try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            byte[] messageDigest = md.digest(password.getBytes());
+
+            BigInteger num = new BigInteger(1, messageDigest);
+
+            String hashedPassword = num.toString(16);
+
+            while (hashedPassword.length() < 32) {
+                hashedPassword = "0" + hash;
+            }
+            return hashedPassword;
+        } catch (NoSuchAlgorithmException nsae) {
+            throw new RuntimeException(nsae);
+        }*/
     }
 }
