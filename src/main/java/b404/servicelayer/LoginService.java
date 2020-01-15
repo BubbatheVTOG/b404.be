a3e8655df1507c1894fbc42af70a0a0759f64d0d
Matthew Marchinetti <mam5588@g.rit.edu>
2020-01-15 16:36:13 -0500

    Further error testing

--- src/main/java/b404/servicelayer/LoginService.java
+++ src/main/java/b404/servicelayer/LoginService.java
@@ -62,7 +62,7 @@ public class LoginService {
         }
         //Catch All to ensure no unexpected internal server errors are being returned to client
         catch(Exception e){
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"" + e.getMessage() + "\"}").build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"" + e.toString() + "\"}").build();
         }
     }
 }
