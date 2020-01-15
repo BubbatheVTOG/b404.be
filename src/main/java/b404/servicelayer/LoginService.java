fd13038a072a7c551c7f6ac2737978afee70cc7d
Matthew Marchinetti <mam5588@g.rit.edu>
2020-01-15 16:03:43 -0500

    Altered Exception response for troubleshooting unexpected exceptions

--- src/main/java/b404/servicelayer/LoginService.java
+++ src/main/java/b404/servicelayer/LoginService.java
@@ -62,7 +62,7 @@ public class LoginService {
         }
         //Catch All to ensure no unexpected internal server errors are being returned to client
         catch(Exception e){
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"" + "An unknown issue has occurred" + "\"}").build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"" + e.getMessage() + "\"}").build();
         }
     }
 }
