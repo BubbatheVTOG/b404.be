55ac4d2a06e703dba14b3f0ab8702581f86f67ba
Matthew Marchinetti <mam5588@g.rit.edu>
2020-01-17 13:41:29 -0500

    Refactored initiating and closing db connection into separate class as it would have been repeated in every data layer class. and made portions of DBConn class package private so it is db connectivity is only usable by datalayer objects

--- src/main/java/b404/datalayer/PersonDB.java
+++ src/main/java/b404/datalayer/PersonDB.java
@@ -6,65 +6,18 @@ import b404.utility.objects.Person;
 import b404.utility.env.EnvManager;
 
 public class PersonDB {
-    private Connection conn;
-
-    private EnvManager env;
-
-    private String driver;
-    private String url;
-    private String user;
-    private String password;
+    DBConn dbConn;
 
     public PersonDB(){
-        this.env = new EnvManager();
-
-        this.driver = "org.mariadb.jdbc.Driver";
-        this.url = "jdbc:mariadb://" + env.getValue("DB_NAME") + ":3306/venture_creations?allowPublicKeyRetrieval=true";
-
-        //TODO: communicate on what these values should be and how best to store them
-        this.user = "b404";
-        this.password = "b404";
-    }
-
-    /**
-     * Opens a connection to the database
-     * Throws a custom SQLException on error
-     */
-    private void connect() throws SQLException{
-        conn = null;
-
-        try{
-            Class.forName(this.driver);
-            conn = DriverManager.getConnection(this.url, this.user, this.password);
-        }
-        //return false on error connecting
-        catch(SQLException sqle){
-            throw new SQLException("Could not connect to database");
-        }
-        catch(ClassNotFoundException cnfe){
-            throw new SQLException("Mariadb driver not found");
-        }
-    }
-
-    /**
-     * Closes the database connection
-     * Throws a custom SQLException on error
-     */
-    private void close() throws SQLException{
-        try{
-            this.conn.close();
-        }
-        catch(SQLException e){
-            throw new SQLException("Error closing the database connection");
-        }
+        this.dbConn = new DBConn();
     }
 
     public Person getPersonByName(String name) throws SQLException {
-        this.connect();
+        this.dbConn.connect();
 
         //Prepare sql statement
         String query = "SELECT * FROM person WHERE person.name = ?";
-        PreparedStatement preparedStatement = this.conn.prepareStatement(query);
+        PreparedStatement preparedStatement = this.dbConn.conn.prepareStatement(query);
 
         //Set parameters and execute query
         preparedStatement.setString(1, name);
@@ -86,7 +39,7 @@ public class PersonDB {
         }
 
         //Close the database
-        this.close();
+        this.dbConn.close();
 
         //return person;
         return person;
