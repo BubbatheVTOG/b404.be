aa8388fcbe9938f6b6b00918a8dd29c329c99a88
Matthew Marchinetti <mam5588@g.rit.edu>
2020-01-18 11:42:10 -0500

    Added GetPerson endpoint in  personService and started work on business layer

--- src/main/java/b404/businesslayer/PersonBusiness.java
+++ src/main/java/b404/businesslayer/PersonBusiness.java
@@ -20,15 +20,52 @@ public class PersonBusiness {
 
     public Person login(String user, String password) throws UnauthorizedException, BadRequestException, InternalServerErrorException{
 
-        Person person = null;
-
         //Initial parameter validation; throws BadRequestException if there is an issue
         if(user == null || user.isEmpty()){ throw new BadRequestException("Invalid username syntax"); }
         if(password == null || password.isEmpty()){ throw new BadRequestException("Invalid password syntax"); }
 
         try{
             //Retrieve the person from the database by name
-            person = personDB.getPersonByName(user);
+            Person person = personDB.getPersonByName(user);
+
+            if(person == null){
+                throw new UnauthorizedException("Invalid login credentials.");
+            }
+
+            //Check that username was found in the database
+            if(person == null){
+                throw new UnauthorizedException("Invalid login credentials.");
+            }
+
+            //Encrypt password that was passed in and compare to hash stored in database
+            //Throw UnauthorizedException if they do not match
+            String salt = person.getSalt();
+            String encryptedPassword = PasswordEncryption.hash(password, salt);
+
+            if(!person.getPasswordHash().equals(encryptedPassword)){
+                throw new UnauthorizedException("Invalid login credentials.");
+            }
+
+            //Reaching this indicates no issues have been met and a success message can be returned
+            return person;
+        }
+        //SQLException - If the data layer throws an SQLException; throw a custom Internal Server Error
+        //ArithmeticException - If the password encryption process fails
+        catch(SQLException | ArithmeticException ex){
+            throw new InternalServerErrorException(ex.getMessage());
+        }
+    }
+
+    public Person getPersonByUserID(String userID) throws UnauthorizedException, BadRequestException, InternalServerErrorException {
+        return new Person();
+    }
+    /*
+        //Initial parameter validation; throws BadRequestException if there is an issue
+        if(userID == null || user.isEmpty()){ throw new BadRequestException("Invalid username syntax"); }
+
+        try{
+            //Retrieve the person from the database by name
+            person = personDB.getPersonByName(userID);
 
             if(person == null){
                 throw new UnauthorizedException("Invalid login credentials.");
@@ -57,4 +94,5 @@ public class PersonBusiness {
         //Reaching this indicates no issues have been met and a success message can be returned
         return person;
     }
+    */
 }
--- src/main/java/b404/servicelayer/PersonService.java
+++ src/main/java/b404/servicelayer/PersonService.java
@@ -1,6 +1,21 @@
 package b404.servicelayer;
 
+import b404.businesslayer.PersonBusiness;
+import b404.utility.customexceptions.InternalServerErrorException;
+import b404.utility.customexceptions.UnauthorizedException;
+import b404.utility.objects.Person;
+import b404.utility.security.JWTUtility;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 // https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-2rd-edition/en/part1/chapter15/securing_jax_rs.html
@@ -10,13 +25,58 @@ import javax.ws.rs.core.Response;
  */
 @Path("person")
 public class PersonService {
+    private PersonBusiness personBusiness = new PersonBusiness();
 
-    //TODO: this is web token code Bubba had added which is not currently being used but is maintained here for future reference
-        /*
-        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        String jws = Jwts.builder().setSubject("Joe").signWith(SignatureAlgorithm.HS256, key).compact();
+    /**
+     * Gets a Person object by userID
+     * @param userID - username from POST request body
+     * @return - HTTP Response: 200 OK for person found and returned
+     *                         400 BAD REQUEST for invalid username or password syntax
+     *                         401 UNAUTHORIZED for invalid JSON Web Token in header
+     *                         500 INTERNAL SERVER ERROR for backend error
+     */
+    @Path("/id/{id}")
+    @GET
+    @Operation(summary = "Login", description = "Authenticates the user by username and password")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "{Success: You have logged in.}"),
+            @ApiResponse(code = 400, message = "{error: Invalid username/password syntax}"),
+            @ApiResponse(code = 401, message = "{error: Invalid login credentials.}"),
+            @ApiResponse(code = 500, message = "{error: Sorry, cannot process your request at this time}")
+    })
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPersonByUserID(@RequestBody(description = "id", required = true) @PathParam("id") String userID,
+                                      @Parameter(in = ParameterIn.HEADER, name = "Authorization") @HeaderParam("Authorization") String JWT) {
+        try {
+            if(!JWTUtility.validateToken(JWT, userID)){
+                throw new UnauthorizedException("Invalid JSON Web Token provided.");
+            }
 
-        assert Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody().getSubject().equals("Joe");
-        */
+            //Send parameters to business layer and store response
+            Person person = personBusiness.getPersonByUserID(userID);
 
+            String jwtToken = JWTUtility.generateToken(Integer.toString(person.getUserID()));
+
+            //If no errors are thrown in the business layer, it was successful and OK response can be sent with message
+            return Response.ok("{\"success\":\"" + person.toString() + "\"}")
+                    .header("Authorization", jwtToken)
+                    .build();
+        }
+        //Catch an UnauthorizedException and return Unauthorized response with message from error
+        catch(UnauthorizedException ue){
+            return Response.status(Response.Status.UNAUTHORIZED).entity("{\"error\":\"" + ue.getMessage() + "\"}").build();
+        }
+        //Catch a BadRequestException and return Bad Request response with message from error
+        catch(b404.utility.customexceptions.BadRequestException bre){
+            return Response.status(Response.Status.BAD_REQUEST).entity("{\"error\":\"" + bre.getMessage() + "\"}").build();
+        }
+        //Catch an InternalServerErrorException and return Internal Server Error response with message from error
+        catch(InternalServerErrorException isee){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"Sorry, could not process your request at this time.\"}").build();
+        }
+        //Catch All to ensure no unexpected internal server errors are being returned to client
+        catch(Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"" + "Sorry, an unexpected issue has occurred." + "\"}").build();
+        }
+    }
 }
