27e7730df9aaf783a50cf5b80483de47cb395156
Matthew Marchinetti <mam5588@g.rit.edu>
2020-01-27 12:39:18 -0500

    Adjusted syntax in ApplicationConfig

--- src/main/java/b404/ApplicationConfig.java
+++ src/main/java/b404/ApplicationConfig.java
@@ -13,7 +13,7 @@ public class ApplicationConfig extends Application {
 
     private Set<Class<?>> getRestResourceClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
-        resources.add(b404.Ping.class);
+        resources.add(Ping.class);
         resources.add(b404.servicelayer.LoginService.class);
         resources.add(b404.servicelayer.PersonService.class);
         resources.add(b404.servicelayer.CompanyService.class);
