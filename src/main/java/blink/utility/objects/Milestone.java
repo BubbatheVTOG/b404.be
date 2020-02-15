2806629ac7ba555a2570bad5abe92502d22eeb02
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-02-15 16:13:14 -0500

    Refactored create.sql to provide most circumstances for milestone completion and changed person information to easily identify the users during testing. Added archived boolean to create.sql Refactored Milestone object to conform better to updated database schema.

--- src/main/java/blink/utility/objects/Milestone.java
+++ src/main/java/blink/utility/objects/Milestone.java
@@ -1,5 +1,7 @@
 package blink.utility.objects;
 
+import com.google.gson.Gson;
+
 import java.sql.Date;
 
 public class Milestone {
@@ -7,8 +9,41 @@ public class Milestone {
     private int orderNumber;
     private String name;
     private String description;
+    private Date createdDate;
+    private Date lastUpdatedDate;
     private Date deliveryDate;
+    private Date completedDate;
+    private boolean archived;
     private int companyID;
+    private transient Gson gson;
+
+    /**
+     * Construct a milestone and provide all information necessary
+     * @param mileStoneID - ID for the milestone
+     * @param orderNumber - Order for the workflow
+     * @param name - name for the workflow
+     * @param description - general description for the workflow
+     * @param createdDate - date workflow was created
+     * @param lastUpdatedDate - date workflow was updated last
+     * @param deliveryDate - date workflow is scheduled for delivery
+     * @param completedDate - date workflow was completed
+     * @param archived - whether the workflow is archived or not
+     * @param companyID - company that the workflow is assigned to
+     */
+    public Milestone(int mileStoneID, int orderNumber, String name, String description, Date createdDate, Date lastUpdatedDate, Date deliveryDate, Date completedDate, int archived, int companyID) {
+        this.mileStoneID = mileStoneID;
+        this.orderNumber = orderNumber;
+        this.name = name;
+        this.description = description;
+        this.createdDate = createdDate;
+        this.lastUpdatedDate = lastUpdatedDate;
+        this.deliveryDate = deliveryDate;
+        this.completedDate = completedDate;
+        this.archived = (archived == 1 ? Boolean.TRUE : Boolean.FALSE);
+        this.companyID = companyID;
+
+        this.gson = new Gson();
+    }
 
     public Milestone(int mileStoneID, int orderNumber, String name, String description, Date deliveryDate, int companyID) {
         this.mileStoneID = mileStoneID;
@@ -17,33 +52,96 @@ public class Milestone {
         this.description = description;
         this.deliveryDate = deliveryDate;
         this.companyID = companyID;
+        this.archived = Boolean.FALSE;
+
+        this.gson = new Gson();
     }
 
-    public Milestone(int mileStoneID) { this.mileStoneID = mileStoneID; }
+    public int getMileStoneID() {
+        return mileStoneID;
+    }
 
-    public Milestone(String name) { this.name = name; }
+    public void setMileStoneID(int mileStoneID) {
+        this.mileStoneID = mileStoneID;
+    }
 
-    public int getMileStoneID() { return mileStoneID; }
+    public int getOrderNumber() {
+        return orderNumber;
+    }
 
-    public void setMileStoneID(int mileStoneID) { this.mileStoneID = mileStoneID; }
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
 
-    public int getOrderNumber() { return orderNumber; }
+    public String getName() {
+        return name;
+    }
 
-    public void setOrderNumber(int orderNumber) { this.orderNumber = orderNumber; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getName() { return name; }
+    public String getDescription() {
+        return description;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public String getDescription() { return description; }
+    public Date getCreatedDate() {
+        return createdDate;
+    }
 
-    public void setDescription(String description) { this.description = description; }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 
-    public Date getDeliveryDate() { return deliveryDate; }
+    public Date getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
 
-    public void setDeliveryDate(Date deliveryDate) { this.deliveryDate = deliveryDate; }
+    public void setLastUpdatedDate(Date lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
 
-    public int getCompanyID() { return companyID; }
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
 
-    public void setCompanyID(int companyID) { this.companyID = companyID; }
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public Date getCompletedDate() {
+        return completedDate;
+    }
+
+    public void setCompletedDate(Date completedDate) {
+        this.completedDate = completedDate;
+    }
+
+    public boolean isCompleted() {
+        return this.deliveryDate == null;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+
+    public int getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(int companyID) {
+        this.companyID = companyID;
+    }
+
+    public String toJSON(){
+        return this.gson.toJson(this);
+    }
 }
