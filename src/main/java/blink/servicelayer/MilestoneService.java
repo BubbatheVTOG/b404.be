3ba1ca845edc6bd0eba1699b4e9fef15b1990181
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-02-17 19:23:51 -0500

    Debugging JWTUtility

--- src/main/java/blink/servicelayer/MilestoneService.java
+++ src/main/java/blink/servicelayer/MilestoneService.java
@@ -45,7 +45,7 @@ public class MilestoneService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllMilestones(@Parameter(in = ParameterIn.HEADER, name = "Authorization") @HeaderParam("Authorization") String jwt) {
         try {
-            Authorization.isLoggedIn(jwt);
+            //Authorization.isLoggedIn(jwt);
 
             //Send parameters to business layer and store response
             List<Milestone> milestoneList = milestoneBusiness.getAllMilestones("c5877b03-ac76-4e71-9a88-1c2d9122d474");
