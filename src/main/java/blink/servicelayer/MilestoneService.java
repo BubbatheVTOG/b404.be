eb4ebdad08896e7bec6cea50d5ca1ef406e210de
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-03-04 21:20:42 -0500

    Debugging getAllMilestones

--- src/main/java/blink/servicelayer/MilestoneService.java
+++ src/main/java/blink/servicelayer/MilestoneService.java
@@ -55,6 +55,12 @@ public class MilestoneService {
             return ResponseBuilder.buildSuccessResponse(gson.toJson(milestoneList));
         }
         //Catch error exceptions and return relevant Response using ResponseBuilder
+        catch(NullPointerException npe){
+            return ResponseBuilder.buildErrorResponse(Response.Status.BAD_REQUEST, "Null Pointer");
+        }
+        catch(InternalServerErrorException npe){
+            return ResponseBuilder.buildErrorResponse(Response.Status.BAD_REQUEST, "Database failure");
+        }
         catch (NotAuthorizedException nae) {
             return ResponseBuilder.buildErrorResponse(Response.Status.UNAUTHORIZED, nae.getMessage());
         } catch (Exception e) {
@@ -89,9 +95,6 @@ public class MilestoneService {
             return ResponseBuilder.buildSuccessResponse(gson.toJson(milestoneList));
         }
         //Catch error exceptions and return relevant Response using ResponseBuilder
-        catch(NullPointerException npe){
-            return ResponseBuilder.buildErrorResponse(Response.Status.BAD_REQUEST, "Null Pointer");
-        }
         catch (NotAuthorizedException nae) {
             return ResponseBuilder.buildErrorResponse(Response.Status.UNAUTHORIZED, nae.getMessage());
         } catch (Exception e) {
