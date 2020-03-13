95281ca4702e773cfe7d09f07baa58b9b8044997
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-03-13 17:57:55 -0400

    Patching user specific getAllWorkflows

--- src/main/java/blink/servicelayer/WorkflowService.java
+++ src/main/java/blink/servicelayer/WorkflowService.java
@@ -59,7 +59,7 @@ public class WorkflowService {
         catch (NotAuthorizedException nae) {
             return ResponseBuilder.buildErrorResponse(Response.Status.UNAUTHORIZED, nae.getMessage());
         } catch (Exception e) {
-            return ResponseBuilder.buildInternalServerErrorResponse();
+            return ResponseBuilder.buildErrorResponse(Response.Status.BAD_REQUEST, e.getMessage());//return ResponseBuilder.buildInternalServerErrorResponse();
         }
     }
 
