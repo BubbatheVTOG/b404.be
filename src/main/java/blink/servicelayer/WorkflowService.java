1f9655b7170bc734ac9c2719ba8dc3598d972e8f
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-03-13 19:47:15 -0400

    Debugging pending tasks

--- src/main/java/blink/servicelayer/WorkflowService.java
+++ src/main/java/blink/servicelayer/WorkflowService.java
@@ -59,7 +59,7 @@ public class WorkflowService {
         catch (NotAuthorizedException nae) {
             return ResponseBuilder.buildErrorResponse(Response.Status.UNAUTHORIZED, nae.getMessage());
         } catch (Exception e) {
-            return ResponseBuilder.buildErrorResponse(Response.Status.BAD_REQUEST, e.getMessage());//return ResponseBuilder.buildInternalServerErrorResponse();
+            return ResponseBuilder.buildInternalServerErrorResponse();
         }
     }
 
@@ -553,7 +553,9 @@ public class WorkflowService {
             return ResponseBuilder.buildErrorResponse(Response.Status.UNAUTHORIZED, nae.getMessage());
         }
         catch(Exception e){
-            return ResponseBuilder.buildInternalServerErrorResponse();
+            return ResponseBuilder.buildErrorResponse(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+            //TODO:Revert this
+            // return ResponseBuilder.buildInternalServerErrorResponse();
         }
     }
 
