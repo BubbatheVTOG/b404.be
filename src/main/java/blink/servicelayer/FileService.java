12f6742fdffd9d336ef6c67255e29e28d9a13498
mam5588 <47454328+mam5588@users.noreply.github.com>
2020-03-31 16:20:52 -0400

    Fixing getAllFilesByMilestone sql query typo

--- src/main/java/blink/servicelayer/FileService.java
+++ src/main/java/blink/servicelayer/FileService.java
@@ -118,8 +118,7 @@ public class FileService {
             return ResponseBuilder.buildErrorResponse(Response.Status.UNAUTHORIZED, nae.getMessage());
         }
         catch(Exception e){
-            return ResponseBuilder.buildErrorResponse(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
-            //return ResponseBuilder.buildInternalServerErrorResponse();
+            return ResponseBuilder.buildInternalServerErrorResponse();
         }
     }
 
